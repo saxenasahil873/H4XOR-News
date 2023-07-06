7aa11421130322a481dccb2b30531d3152abd581
Sahil Saxena <122679806+saxenasahil873@users.noreply.github.com>
2023-07-06 11:52:53 -0700

    Add files via upload

---
+++ H4X0R News/H4X0R News/Views/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Sahil Saxena on 08/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+            }
+            .navigationBarTitle("H4X0R NEWS")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
+//
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Bonjour"),
+//    Post(id: "3", title: "Hola")
+//]
